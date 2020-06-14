ec31de1e04adef757cf88891f11006526d3a710d
OUARRICH <warichsaid@gmail.com>
2020-06-14 11:47:05 +0200

    feat: add avatar

--- src/menu/Menu.component.js
+++ src/menu/Menu.component.js
@@ -7,17 +7,26 @@ import {menuItems} from './items';
 import './Menu.component.css';
 
 const Menu = ({open, onClickMenuItem, selectedMenuItem}) => (
-    <ul className={classNames("App__menu", {"App__menu--open": open}, {"App__menu--close": !open})}>
-        {
-        menuItems.map(item => <Link to={item.url} key={`Link__${item.value}`}>
-            <li onClick={() => onClickMenuItem(item)} key={item.value} className={classNames("App__menu__item", 
-                {"App__menu__item--selected": item.value === selectedMenuItem}
-            )}>
-                {item.text}
-            </li>
-        </Link>)
-        }
-    </ul>
+    <div className={classNames("App__menu", {"App__menu--open": open}, {"App__menu--close": !open})}>
+        <div className="App__menu__avatarContainer">
+            <div className="App__menu__avatar"></div>
+            <h2>Said OUARRICH</h2>
+            <p>Développeur JavaScript</p>
+            <p>NodeJS</p>
+            <p>React - Angular</p>
+        </div>
+        <ul>
+            {
+            menuItems.map(item => <Link to={item.url} key={`Link__${item.value}`}>
+                <li onClick={() => onClickMenuItem(item)} key={item.value} className={classNames("App__menu__item", 
+                    {"App__menu__item--selected": item.value === selectedMenuItem}
+                )}>
+                    {item.text}
+                </li>
+            </Link>)
+            }
+        </ul>
+    </div>
 );
 
 export default Menu;
